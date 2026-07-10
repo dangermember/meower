f4041d2fb847c73f449f33e68c5f2daf1a2985c1
Mohamed Eid Rashad <eng.mohamedeid2010@gmail.com>
2026-07-10 08:33:10 +0300

    GET / and POST /meows

--- server/index.js
+++ server/index.js
@@ -2,6 +2,16 @@ const express = require('express');
 
 const app = express();
 
+app.get('/',(req,res)=>{
+    res.json({
+        message:"meower 😺"
+    })
+});
+
+app.post('/meows',(req,res)=>{
+    console.log(req.body);
+})
+
 app.listen(5000,()=>{
     console.log('Listening on http://localhost:5000');
 })
